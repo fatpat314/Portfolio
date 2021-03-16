5296055df19e909dfcb23e9ecc073380ed3e03de
Patrick <w.patrick.kelly@gmail.com>
2021-03-15 18:25:28 -0700

    changed port call for heroku test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs')
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
