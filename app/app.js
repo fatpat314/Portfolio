d643c8be9b83a4eed018c4c0098cf1060b6babbb
Patrick <w.patrick.kelly@gmail.com>
2021-03-15 17:57:17 -0700

    src to app for heroku

--- app/app.js
+++ app/app.js
@@ -2,13 +2,13 @@ const http = require('http');
 const fs = require('fs')
 
 const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('index.html').pipe(res)
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${PORT}/`);
 });
